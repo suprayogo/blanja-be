7c71743799c71c7c81e1960ea5cff54930aaf3f9
NLS <naufalls21@gmail.com>
2023-07-24 13:39:51 +0700

    clean repo

--- routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const authController = require("../controllers/auth.controller");
-
-router.post("/customer/login", authController.loginCustomer);
-
-router.post("/seller/login", authController.loginSeller);
-
-module.exports = router;
